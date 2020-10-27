e1d500ac39ee1f7f315917ab4372590842c93c45
高振翔_time <xiangz.gao@timemicro.tech>
2020-10-27 14:16:03 +0800

    up

--- test.js
+++ test.js
@@ -1,22 +1,7 @@
 const checkDto = require('./util/checkDto')
 
 const test = () => {
-    const dto = require('./dto/user/create.dto').request
-    let testData = {
-        username: 'test',
-        phone: 17855830799,
-        age: 100,
-        isDelete: false,
-        list: [1,2,3],
-        obj: {
-            a: 1,
-            b: {
-                c: 1,
-                d: [1,2,3]
-            }
-        }
-    }
-    checkDto(dto, testData, 'request');
-    console.log(testData)
+    const dto = require('./dto');
+    console.log(dto)
 }
 test()
